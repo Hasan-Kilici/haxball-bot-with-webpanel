c853dc09ea7d807976c3d01eda5ad00d94ef8acd
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2024-01-24 20:51:55 +0300

    Delete commands directory

--- commands/giant.js
+++
@@ -1,8 +0,0 @@
-module.exports = (player, message, room)=>{
-    if(player.admin){
-        let users = room.getPlayerList()
-        for(let i = 0;i < users.length;i++){
-            room.setPlayerDiscProperties(users[i].id, {radius: 50})
-        }
-    }
-}
--- commands/mute.js
+++
@@ -1,7 +0,0 @@
-module.exports = (player, message, room, mute)=>{
-    let users = room.getPlayerList();
-    if (player.admin) {
-        let muted = users.find((p) => p.id == command[1])
-        mute(muted.name, 2000)
-    }
-}
